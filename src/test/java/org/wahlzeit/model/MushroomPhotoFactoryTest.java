29d1affbc13ae662daef1b463054a5120976f874
Kai T <kai.teichmann@fau.de>
2019-01-19 22:56:05 +0100

    added missing classrules to MushroomPhotoFactoryTest

--- src/test/java/org/wahlzeit/model/MushroomPhotoFactoryTest.java
+++ src/test/java/org/wahlzeit/model/MushroomPhotoFactoryTest.java
@@ -21,10 +21,26 @@
 package org.wahlzeit.model;
 
 import static org.junit.Assert.*;
+
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.wahlzeit.testEnvironmentProvider.LocalDatastoreServiceTestConfigProvider;
+import org.wahlzeit.testEnvironmentProvider.RegisteredOfyEnvironmentProvider;
+import org.wahlzeit.testEnvironmentProvider.SysConfigProvider;
+import org.wahlzeit.testEnvironmentProvider.UserServiceProvider;
+import org.wahlzeit.testEnvironmentProvider.UserSessionProvider;
 
 public class MushroomPhotoFactoryTest {
 
+	@ClassRule
+	public static RuleChain ruleChain = RuleChain.
+			outerRule(new LocalDatastoreServiceTestConfigProvider()).
+			around(new RegisteredOfyEnvironmentProvider()).
+			around(new SysConfigProvider()).
+			around(new UserServiceProvider()).
+			around(new UserSessionProvider());
+	
 	@Test
 	public void testInitialize() {
 		MushroomPhotoFactory.initialize();
